82afc1594965d7aea103ce761b76122f241b7735
Uboatwaffe <mpiatek1512@gmail.com>
2023-05-12 19:27:57 +0200

    Encoding and decoding works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,32 +9,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //Variables
-        String key;
         String word;
-        char changing;
-        char setKey;
 
 
 
-            //Asks user for key
-            System.out.print("Insert key: ");
-            key = sc.nextLine();
 
-            setKey = key.charAt(0);
 
         while(true) {
-            Encryption one = new Encryption(setKey);
+            Encryption one = new Encryption();
             System.out.print("Insert text to be encrypted (or type 'STOP' to exit): ");
             word = sc.nextLine();
             if (!Objects.equals(word, "STOP")) {
                 for (int i = 0; i < word.length(); i++) {
                     System.out.print(
-                            one.EncryptThree(
-                                    one.EncryptTwo(
-                                            one.EncryptOne(
-                                                    word.charAt(i)
-                                            )
-                                    )
+                            one.EncryptOne(
+                                    word.charAt(i)
                             )
                     );
                 }
